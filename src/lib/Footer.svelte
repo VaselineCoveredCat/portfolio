79193f372db667daa81f2c2d9272e2bd5d1a2ec5
Firas <VaselineCoveredCat@users.noreply.github.com>
2022-06-29 14:36:06 +0700

    Create Footer.svelte

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,31 @@
+<footer>
+  <p>&copy; 2022 Vaseline Covered Cat</p>
+</footer>
+
+<style>
+  footer {
+    padding: 6vw;
+    text-align: end;
+  }
+
+  /* 640 */
+  @media (min-width: 40em) {
+    footer {
+      padding: 4vw;
+    }
+  }
+
+  /* 1280 */
+  @media (min-width: 80em) {
+    footer {
+      font-size: var(--small);
+    }
+  }
+
+  /* 1920 */
+  @media (min-width: 120em) {
+    footer {
+      padding: 77px;
+    }
+  }
+</style>
