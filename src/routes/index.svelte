dce077d3fc73695ed951855c3fae7a05948ccf5a
Firas <VaselineCoveredCat@users.noreply.github.com>
2022-07-01 18:23:03 +0700

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,40 @@
 <script>
-  import Navbar from "$lib/Nav.svelte";
+  import Avatar from "$lib/Avatar.svelte";
   import Footer from "$lib/Footer.svelte";
+  import Nav from "$lib/Nav.svelte";
+  import Projects from "$lib/Projects.svelte";
 </script>
 
 <header>
-  <Navbar />
-
-  <!-- dont forget to upload a photo -->
-  <picture aria-hidden="true">
-    <img
-      src="https://images.unsplash.com/photo-1559794689-c4f815791a80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw3VUVpN0g5LVhFNHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=640&q=60"
-      alt=""
-      encoding="async"
-    />
-  </picture>
-
+  <Nav />
+  <Avatar />
   <h1>Firas Hasnadong</h1>
-
   <p>
     Front end web developer based in Jakarta, here you can check out what I'm
     working on.
   </p>
 </header>
-
+<main>
+  <h2>Latest work</h2>
+  <Projects />
+</main>
 <Footer />
 
 <style>
-  header {
+  header,
+  main {
     display: grid;
     grid-template-columns: minmax(0, 1fr);
-    grid-template-rows: auto minmax(0, 1fr) auto;
     gap: 6vw;
   }
 
-  picture {
-    width: 70%;
-    place-self: center;
-    grid-area: 2/1;
+  main,
+  p {
+    padding: 0 6vw;
   }
 
-  img {
-    aspect-ratio: 2/3;
+  main {
+    margin-top: 6vw;
   }
 
   h1 {
@@ -50,46 +44,41 @@
     text-transform: uppercase;
   }
 
+  h2 {
+    position: absolute;
+    top: auto;
+    left: -10000px;
+    width: 1px;
+    height: 1px;
+    overflow: hidden;
+  }
+
   p {
-    padding: 0 6vw;
     font-size: var(--h5);
   }
 
   /* 640 */
   @media (min-width: 40em) {
-    header {
-      gap: 4vw;
-    }
-
-    picture {
-      width: 50%;
-    }
-
-    p {
-      padding: 0 4vw;
-    }
   }
 
   /* 1280 */
   @media (min-width: 80em) {
-    picture {
-      width: 30%;
-    }
-
     p {
-      font-size: var(--h3);
       max-width: 80%;
+      font-size: var(--h3);
     }
   }
 
   /* 1920 */
   @media (min-width: 120em) {
-    header {
-      gap: 77px;
+    header,
+    main {
+      gap: 115px;
     }
 
+    main,
     p {
-      padding: 0 77px;
+      padding: 0 115px;
     }
   }
 </style>
