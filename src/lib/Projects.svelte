2b2026836c59a7f01451b667b089b664d423068e
Firas <VaselineCoveredCat@users.noreply.github.com>
2022-07-01 18:22:46 +0700

    Create Projects.svelte

---
+++ src/lib/Projects.svelte
@@ -0,0 +1,101 @@
+<ul role="list">
+  <li>
+    <a href="https://vaselinecoveredcat.github.io/untitled-project-2/">
+      <picture>
+        <source
+          media="(min-width: 1920px)"
+          srcset="https://toytoytot.imgix.net/https%3A%2F%2Fi.imgur.com%2FPANVCKU.jpg?auto=format&w=1920&fit=crop&s=90b883544106595960ebf3b686bac61e"
+        />
+        <source
+          media="(min-width: 1600px)"
+          srcset="https://toytoytot.imgix.net/https%3A%2F%2Fi.imgur.com%2FPANVCKU.jpg?auto=format&w=1600&fit=crop&s=e66a3fa128f7ce8ddb046c17da20573b"
+        />
+        <source
+          media="(min-width: 1280px)"
+          srcset="https://toytoytot.imgix.net/https%3A%2F%2Fi.imgur.com%2FPANVCKU.jpg?auto=format&w=1280&fit=crop&s=3c3c6da096ab2bdfe3a5fbb5d5ffd9ef"
+        />
+        <source
+          media="(min-width: 960px)"
+          srcset="https://toytoytot.imgix.net/https%3A%2F%2Fi.imgur.com%2FPANVCKU.jpg?auto=format&w=960&fit=crop&s=5468a696e118814591f90fd9d0d87c31"
+        />
+        <source
+          media="(min-width: 640px)"
+          srcset="https://toytoytot.imgix.net/https%3A%2F%2Fi.imgur.com%2FPANVCKU.jpg?auto=format&w=640&fit=crop&s=0c84b9e417a33288609758b99a088ff2"
+        />
+        <img
+          src="https://toytoytot.imgix.net/https%3A%2F%2Fi.imgur.com%2FPANVCKU.jpg?auto=format&w=320&fit=crop&s=cb8663b285ffd8e43514bd10393f5c75"
+          alt="Untitled project 2"
+          loading="lazy"
+          encoding="async"
+        />
+      </picture>
+    </a>
+  </li>
+
+  <li>
+    <a href="https://vaselinecoveredcat.github.io/untitled-project-1/">
+      <picture>
+        <source
+          media="(min-width: 1920px)"
+          srcset="https://toytoytot.imgix.net/https%3A%2F%2Fi.imgur.com%2FL4LnfEg.jpg?auto=format&w=1920&fit=crop&s=f7c45ba710930eaf0230304f263f50dc"
+        />
+        <source
+          media="(min-width: 1600px)"
+          srcset="https://toytoytot.imgix.net/https%3A%2F%2Fi.imgur.com%2FL4LnfEg.jpg?auto=format&w=1600&fit=crop&s=742019deb1af20bf210773cd41864368"
+        />
+        <source
+          media="(min-width: 1280px)"
+          srcset="https://toytoytot.imgix.net/https%3A%2F%2Fi.imgur.com%2FL4LnfEg.jpg?auto=format&w=1280&fit=crop&s=fdcdbbe78e741053488836da0e6424d2"
+        />
+        <source
+          media="(min-width: 960px)"
+          srcset="https://toytoytot.imgix.net/https%3A%2F%2Fi.imgur.com%2FL4LnfEg.jpg?auto=format&w=960&fit=crop&s=c590fc514a73d33d9d6830d47df33e87"
+        />
+        <source
+          media="(min-width: 640px)"
+          srcset="https://toytoytot.imgix.net/https%3A%2F%2Fi.imgur.com%2FL4LnfEg.jpg?auto=format&w=640&fit=crop&s=5015b7a9260c2b3ea7fbf14ee845946d"
+        />
+        <img
+          src="https://toytoytot.imgix.net/https%3A%2F%2Fi.imgur.com%2FL4LnfEg.jpg?auto=format&w=320&fit=crop&s=00b1b9c6350283ebc4d32e450515d692"
+          alt="Untitled project 1"
+          loading="lazy"
+          encoding="async"
+        />
+      </picture>
+    </a>
+  </li>
+</ul>
+
+<style>
+  ul {
+    display: grid;
+    grid-template-columns: minmax(0, 1fr);
+    gap: 6vw;
+  }
+
+  li {
+    overflow: hidden;
+  }
+
+  img {
+    aspect-ratio: 1/1;
+    transition: all 0.5s cubic-bezier(0.445, 0.05, 0.55, 0.95);
+  }
+
+  img:hover {
+    transform: scale(1.2);
+  }
+
+  @media (prefers-reduced-motion: reduce) {
+    img {
+      transition: none;
+    }
+  }
+
+  /* 640 */
+  @media (min-width: 40em) {
+    img {
+      aspect-ratio: 21/9;
+    }
+  }
+</style>
