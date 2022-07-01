e87a4058a87383fa75bfaaeb11571235a9a9d9e0
Firas <VaselineCoveredCat@users.noreply.github.com>
2022-07-01 18:21:58 +0700

    Create Avatar.svelte

---
+++ src/lib/Avatar.svelte
@@ -0,0 +1,53 @@
+<picture aria-hidden="true">
+  <source
+    media="(min-resolution: 144dpi) and (min-width: 1920px),(-webkit-min-device-pixel-ratio: 1.5) and (min-width: 1920px)"
+    srcset="https://toytoytot.imgix.net/https%3A%2F%2Fi.imgur.com%2FM9aBmWJ.jpg?auto=format&w=580&dpr=2&fit=crop&s=dd0eeb3ad4a4644a8d0918f78f579786"
+  />
+  <source
+    media="(min-resolution: 144dpi) and (min-width: 1280px),(-webkit-min-device-pixel-ratio: 1.5) and (min-width: 1280px)"
+    srcset="https://toytoytot.imgix.net/https%3A%2F%2Fi.imgur.com%2FM9aBmWJ.jpg?auto=format&w=380&dpr=2&fit=crop&s=ba94a29d97bf02a116276b5d02801f64"
+  />
+  <source
+    media="(min-resolution: 144dpi) and (min-width: 240px),(-webkit-min-device-pixel-ratio: 1.5) and (min-width: 240px)"
+    srcset="https://toytoytot.imgix.net/https%3A%2F%2Fi.imgur.com%2FM9aBmWJ.jpg?auto=format&w=240&dpr=2&fit=crop&s=88339cdc24fce3acec73dd06d9830978"
+  />
+  <source
+    media="(min-width: 1920px)"
+    srcset="https://toytoytot.imgix.net/https%3A%2F%2Fi.imgur.com%2FM9aBmWJ.jpg?auto=format&w=580&dpr=1&fit=crop&s=983bbcbb0cbe5ea89d6c27a1c06f61a9"
+  />
+  <source
+    media="(min-width: 1280px)"
+    srcset="https://toytoytot.imgix.net/https%3A%2F%2Fi.imgur.com%2FM9aBmWJ.jpg?auto=format&w=380&dpr=1&fit=crop&s=2c03e24b357226d6e3905d241bcddf67"
+  />
+  <img
+    src="https://toytoytot.imgix.net/https%3A%2F%2Fi.imgur.com%2FM9aBmWJ.jpg?auto=format&w=240&dpr=1&fit=crop&s=43ae4eb3caf8346c9e62907d8c5393a5"
+    alt=""
+    encoding="async"
+  />
+</picture>
+
+<style>
+  picture {
+    grid-area: 2/1;
+    place-self: center;
+    width: 70%;
+  }
+
+  img {
+    aspect-ratio: 2/3;
+  }
+
+  /* 640 */
+  @media (min-width: 40em) {
+    picture {
+      width: 50%;
+    }
+  }
+
+  /* 1280 */
+  @media (min-width: 80em) {
+    picture {
+      width: 30%;
+    }
+  }
+</style>
