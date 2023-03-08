6682fe24b1cb7e1dc6b1c5c91cfabca290eedf41
Firas <VaselineCoveredCat@users.noreply.github.com>
2023-03-08 17:19:35 +0700

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<div class="grid grid-r-screen min-h-screen">
+  <slot />
+
+  <footer aria-hidden="true" class="px-s-2xl py-s">
+    <p>
+      <a
+        href="https://github.com/VaselineCoveredCat"
+        rel="noopener noreferrer"
+        target="_blank">Github</a
+      >
+    </p>
+  </footer>
+</div>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,21 @@
+<svelte:head>
+  <title>About - Portfolio</title>
+</svelte:head>
+
+<header class="px-s-2xl py-s">
+  <nav class="text-right">
+    <a href="/">Home</a>
+  </nav>
+</header>
+
+<main class="px-s-2xl">
+  <article>
+    <h1 class="mb-xl text-uppercase">About</h1>
+    <p class="max-w-prose">
+      Hey, it's me Firas, a front end web developer from Indonesia. Currently I
+      live in South Jakarta area and I am available for work. I'm looking
+      forward to contribute my skill&mdash;HTML5, CSS3, NextJS&mdash;in your
+      projects and be a part of the solution.
+    </p>
+  </article>
+</main>
