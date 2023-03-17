6184332673346c0a865d25fa02f9b86d70a7a835
Firas <VaselineCoveredCat@users.noreply.github.com>
2023-03-17 08:54:50 +0700

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+const lenis = new Lenis({ wheelMultiplier: 1.5 });
+const mediaQuery = window.matchMedia("(prefers-reduced-motion: no-preference)");
+
+function raf() {
+  lenis.raf();
+  requestAnimationFrame(raf);
+}
+
+if (mediaQuery.matches) {
+  requestAnimationFrame(raf);
+} else {
+  lenis.stop();
+  lenis.destroy();
+}
